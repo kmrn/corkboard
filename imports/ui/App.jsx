8195de31f6a9e4d5797c22bf42f134bc9b06e3d5
kmrn <hello@kamranpayne.com>
2019-05-17 23:38:13 -0400

    set up meteor with react

---
+++ imports/ui/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Corkboard from './Corkboard.jsx';
+
+const App = () => (
+  <div>
+    <Header />
+    <Corkboard />
+    <Footer />
+  </div>
+);
+
+export default App;
---
+++ imports/ui/Corkboard.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Post from './Post.jsx';
+
+export default class Corkboard extends Component {
+    getPosts() {
+        return [
+            { _id: 1, title: "Hey", text: "Hello world" },
+            { _id: 2, title: "Uh huh", text: "this is another post" },
+            { _id: 3, title: "big boy", text: "Here is some other stuff going on" }
+        ];
+    }
+
+    renderPosts() {
+        return this.getPosts().map((post) => (
+            <Post key={post._id} post={post} />
+        ));
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <ul>
+                    {this.renderPosts()}
+                </ul>
+            </div>
+        );
+    }
+}
